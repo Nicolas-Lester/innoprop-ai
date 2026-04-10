1becd265f3ae9f2cb2085269a852a21c3fd5a36e
Nicolas <nicolasherreramontes625@gmail.com>
2026-04-10 19:10:12 -0400

    feat: actualizar la configuración de Prisma para usar DIRECT_URL en lugar de DATABASE_URL

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from '@prisma/config';
 import * as dotenv from 'dotenv';
 import path from 'path';
 
-// Esto busca el .env dos carpetas arriba (en la raíz del monorepo)
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
-    url: process.env.DATABASE_URL,
+    // IMPORTANTE: Para el comando db:push, usa la DIRECT_URL (puerto 5432)
+    // Esto evita que el Pooler bloquee la creación de tablas
+    url: process.env.DIRECT_URL, 
   },
 });
