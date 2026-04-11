598a5704dd12be1de8322be5d2c9b3b13bb8a70e
Nicolas <nicolasherreramontes625@gmail.com>
2026-04-10 20:29:14 -0400

    feat: architecture and db connection done

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 1. Habilitamos CORS para que el puerto 3000 pueda hablar con el 4000 sin bloqueos de seguridad
+  // 1. Habilitamos CORS para que el puerto 3000 pueda hablar con el 4000 sin bloqueos de seguridad del navegador
   app.enableCors();
 
   // 2. Cambiamos el puerto por defecto al 4000
