1d2ed539a6b8e843710d5e5ffbe9d5016d67f25f
Nicolas <nicolasherreramontes625@gmail.com>
2026-04-10 20:28:01 -0400

    feat: habilitar CORS y cambiar puerto por defecto a 4000 en main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 1. Habilitamos CORS para que el puerto 3000 pueda hablar con el 4000 sin bloqueos de seguridad
+  app.enableCors();
+
+  // 2. Cambiamos el puerto por defecto al 4000
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
