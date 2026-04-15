58d13b2494168b72f10a66cde9e13a1830e4198e
Nicolas <nicolasherreramontes625@gmail.com>
2026-04-15 18:02:33 -0400

    feat: agregar bcrypt y tipos para manejo de contraseñas en el registro de usuarios

---
+++ apps/api/src/dto/register.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @ApiProperty({ example: 'nicolas@ejemplo.com' })
+  @IsEmail({}, { message: 'El email no es válido' })
+  email: string;
+
+  @ApiProperty({ example: '123456' })
+  @IsString()
+  @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
+  password: string;
+
+  @ApiProperty({ example: 'Nicolás Herrera' })
+  @IsString()
+  name: string;
+}
