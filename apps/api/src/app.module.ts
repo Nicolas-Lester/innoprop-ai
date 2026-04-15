081153b76d9fbbc7390dd75bcb9d5874822dd74a
Nicolas <nicolasherreramontes625@gmail.com>
2026-04-15 18:20:56 -0400

    refactor: reorganizar importaciones y mejorar la configuración del módulo AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,32 +1,44 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'; // Importa esto
+import { JwtModule } from '@nestjs/jwt'; // <-- Este es el que te falta para el error rojo
+import { ConfigModule } from '@nestjs/config'; // <-- Para el ConfigModule
+import { ThrottlerModule } from '@nestjs/throttler';
+import * as path from 'path'; // <-- Importante para que funcione el path.join
+
+// Controllers
 import { AppController } from './app.controller';
+import { AuthController } from './auth.controller';
+
+// Services
 import { AppService } from './app.service';
+import { AuthService } from './auth.service';
 import { PrismaService } from './prisma.service';
 import { AIService } from './ai.service';
 import { EmailService } from './email.service';
-import * as path from 'path';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'SUPER_SECRET_KEY',
+      signOptions: { expiresIn: '1d' },
+    }),
     ConfigModule.forRoot({
-      // Esto le dice a Nest que busque el .env dos carpetas arriba (en la raíz)
       envFilePath: path.join(__dirname, '../../../.env'),
       isGlobal: true,
     }),
-    ThrottlerModule.forRoot([
-      {
-        // <--- Agrega el corchete aquí
-        ttl: 60000,
-        limit: 10,
-      },
-    ]),
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
   ],
   controllers: [AppController, AuthController],
-  providers: [AppService, PrismaService, AIService, EmailService, AuthService, JwtStrategy],
+  providers: [
+    AppService,
+    PrismaService,
+    AIService,
+    EmailService,
+    AuthService,
+    JwtStrategy,
+  ],
 })
 export class AppModule {}
